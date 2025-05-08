3af762620f2e9f160aec1ff4fe75478b9ba0319f
tcsgik <tcsgik@gamil.com>
2025-05-08 14:17:34 -0400

    Updated Foo.py with HelloWorld class and example usage

---
+++ Foo.py
@@ -0,0 +1,8 @@
+class HelloWorld:
+    def __init__(self):
+        # Print "Hello World" when an instance is created
+        print("Hello World")
+
+# Example usage:
+# hw = HelloWorld()ls
+HelloWorld()
